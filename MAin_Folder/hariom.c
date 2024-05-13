9ba6478895056529eb36ddb918d2e38a54a923a2
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2024-05-13 21:13:07 +0545

    hariom

---
+++ MAin_Folder/hariom.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int count = 10;
+    for (int a = 0; a < count; a++)
+    {
+        /* code */
+        printf("Hello World\n");
+        printf("%d\n", a);
+    }
+    return 0;
+}
