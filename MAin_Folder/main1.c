b037790a4913e7ffcb6ede4dafc679789141f828
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2024-05-13 19:40:38 +0545

    file added

---
+++ MAin_Folder/main1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int count = 10;
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+        printf("Hello World\n");
+        
+    }
+    
+    return 0;
+}
